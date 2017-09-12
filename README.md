d8af5598586ce20793aa0907b6910e7471bba289
Fernando Vilas Maciel <vilasmaciel@gmail.com>
2017-09-12 08:37:10 +0200

    Change default user

--- README.md
+++ README.md
@@ -1,4 +1,9 @@
 # mysql-docker-multiple-databases
 Mysql docker image for testing and continuous integration proposes
 
-This container executes a MySql instance creating a default user 'test', with password 'test', and three databases: test1, test2 and test3
+This container executes a MySql instance creating:
+ *a default user: `testUser ` with password `testPass`
+ *three databases:
+ **test1
+ **test2
+ **test3
--- create-databases.sql
+++ create-databases.sql
@@ -2,8 +2,8 @@
 CREATE  DATABASE IF NOT EXISTS test1;
 CREATE  DATABASE test2;
 CREATE  DATABASE test3;
-CREATE USER 'test'@'%' IDENTIFIED BY 'test';
-GRANT ALL ON test1.* TO 'test'@'%';
-GRANT ALL ON test2.* TO 'test'@'%';
-GRANT ALL ON test3.* TO 'test'@'%';
+CREATE USER 'testUser'@'%' IDENTIFIED BY 'testPass';
+GRANT ALL ON test1.* TO 'testUser'@'%';
+GRANT ALL ON test2.* TO 'testUser'@'%';
+GRANT ALL ON test3.* TO 'testUser'@'%';
 FLUSH PRIVILEGES ;
